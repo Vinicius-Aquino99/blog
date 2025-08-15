fc7aa3478360316a82ed22ba6c9a95846d0f9f4a
Vinícius Aquino <vnaquino99@gmail.com>
2025-08-15 14:16:17 -0300

    fixed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,62 @@
+import { Router, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+
 import AdminPage from "./components/AdminPage";
 import LoginPage from "./components/LoginPage";
 import PublicPage from "./components/PublicPage";
-import EditPost from "./components/EditPost"
+import EditPost from "./components/EditPost";
 import CreatePost from "./components/CreatePost";
-import { HashRouter as Router, Routes, Route, Link } from 
-'react-router-dom';
-import { useState } from "react"
 import PostContent from "./components/PostContent";
+import PrivateRoute from "./components/PrivateRoute"; // importar
 
 function App() {
   const [isLogged, setIsLogged] = useState(() => {
-  return !!localStorage.getItem("token");
-})
-
+    return !!localStorage.getItem("token");
+  });
 
   return (
     <div className="bg-stone-100 h-screen">
-      <Router>
-
-
-        <Routes>
-          <Route path="/blog" element={<PublicPage />} />
-          <Route path="/login" element={<LoginPage setIsLogged={setIsLogged}/>} />
-          <Route path={`/post/:id`} element={isLogged && <PostContent />} />
-          <Route path='/admin' element={isLogged &&  <AdminPage isLogged={isLogged} setIsLogged={setIsLogged}/>} />
-          <Route path='/edit/:id' element={isLogged &&  <EditPost isLogged={isLogged} />} />
-          <Route path='/create-post' element={isLogged && <CreatePost />} />
-        </Routes>
-      </Router>
-    </div>
     
-  )
+        <Routes>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage setIsLogged={setIsLogged} />} />
+
+          <Route
+            path="/post/:id"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <PostContent />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <AdminPage isLogged={isLogged} setIsLogged={setIsLogged} />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/edit/:id"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <EditPost />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/create-post"
+            element={
+              <PrivateRoute isLogged={isLogged}>
+                <CreatePost />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
